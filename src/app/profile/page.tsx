95ed5be74cca9f89a49b24cd6b5e2301903ce2f3
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2023-07-24 16:44:52 +0530

    middleware complete

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,37 @@
+"use client"
+import axios from"axios"
+import Link from "next/Link"
+import {useRouter} from "next/navigation"
+import React,{useState} from "react"
+
+
 export default function  userProfile(){
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const router = useRouter();
+const[data,setData]=useState("nothing")
+
+const logout= async()=>{
+try{
+
+const logoutData = await axios.get('/api/users/logout')
+console.log(logoutData)
+router.push("/login");
+
+}
+catch(error:any){
+  console.log(error);
+console.log(error);
+
+}
+
+
+}
+
+const getUserDetails = async () => {
+  const res = await axios.get('/api/users/me')
+  console.log(res.data);
+  setData(res.data.data._id)
+}
 
   return (
     <>
@@ -6,6 +39,17 @@ export default function  userProfile(){
     <h1>Profile</h1>
     <hr/>
     <p>Profile Page</p>
+  
+    <h2 className="p-1 rounded bg-gray-900">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}
+            </Link>}</h2>
+
+    <button 
+    onClick={logout}
+    className="bg-slate-900 mt-4 p-2 text-white font-bold px-4 rounded-lg"> Logout</button>
+    
+    <button 
+    onClick={getUserDetails}
+    className="bg-green-900 mt-4 p-2 text-white font-bold px-4 rounded-lg"> Get User Details</button>
     </div>
     
     </>
