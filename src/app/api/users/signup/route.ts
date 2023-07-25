d665d9a98f9c6d0547e1dd54c8fdc33f468e8aee
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2023-07-25 17:55:32 +0530

    verify-user-completed

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connect } from "@/dbconfig/dbconfig";
 import User from "@/models/userModel"
 import { NextRequest,NextResponse } from "next/server";
 import bcryptjs from "bcryptjs" 
+import { sendEmail } from "@/helpers/mailer";
 
 connect();
 
@@ -38,13 +39,17 @@ const newUser = new User(
 )
 
 const savedUser = await newUser.save()
+console.log(savedUser);
+
+//send verification email
+
+await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
 
 return NextResponse.json({
-    message :"User Created Successfully" ,
-    success :true,
+    message: "User created successfully",
+    success: true,
     savedUser
-
-} )
+})
 
 
 
